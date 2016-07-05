1066cbdbcea0407897ba5f84d5d688d941200ab4
System Administrator <root@Stefan-Kempf.local>
2016-07-05 18:44:15 +0200

    weitere Änderungen

---
+++ app/scripts/script.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([30, 0], 2);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
--- grunt/contrib-uglify.js
+++ grunt/contrib-uglify.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
           },
           files: {
               'generated/scripts/script.js': ['app/scripts/*.js'],
-              'generated/scripts/vendor.js': ['bower_components/angular/angular.min.js', 'bower_components/angular-i18n/angular-locale_de-ch.js']
+              'generated/scripts/vendor.js': ['bower_components/angular/angular.min.js', 'bower_components/angular-i18n/angular-locale_de-ch.js', 'bower_components/leaflet/dist/leaflet.js']
 
           }
       },
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
           },
           files: {
               'dist/scripts/script.js': ['app/scripts/*.js'],
-              'dist/scripts/vendor.js': ['bower_components/angular/angular.min.js', 'bower_components/angular-i18n/angular-locale_de-ch.js']
+              'dist/scripts/vendor.js': ['bower_components/angular/angular.min.js', 'bower_components/angular-i18n/angular-locale_de-ch.js', 'bower_components/leaflet/dist/leaflet.js']
           }
       }
   });
