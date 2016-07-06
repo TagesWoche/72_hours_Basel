60f3888658a418c9c0674e81d4b53dc003c03bec
Felix Michel <felix_michel@gmx.ch>
2016-07-06 15:25:43 +0200

    complete refactor of app

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name projekteApp
+ * @description
+ * # projekteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('projekteApp', [
+    'ngResource',
+    'ngSanitize'
+  ]);
