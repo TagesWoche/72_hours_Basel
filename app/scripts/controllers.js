2426b9a8eaeed5ddb8154b45ba299fd723d67a40
Felix Michel <felix_michel@gmx.ch>
2016-07-07 18:23:55 +0200

    Merge branch 'master' of https://github.com/tageswoche/72_hours_Basel
    # Conflicts:
    #	app/index.html
    #	app/styles/sass/style.scss
    #	bower.json
    #	grunt/contrib-uglify.js

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -3,13 +3,6 @@
 angular.module('projekteApp')
 
   .controller('MarkersSimpleController', [ '$scope', function($scope) {
-    var mainMarker = {
-      lat: 47.575,
-      lng: 7.60,
-      focus: true,
-      message: 'Hey, drag me if you want',
-      draggable: true
-    };
 
     angular.extend($scope, {
       basel: {
@@ -18,7 +11,20 @@ angular.module('projekteApp')
         zoom: 12
       },
       markers: {
-        mainMarker: angular.copy(mainMarker)
+          bar1: {
+              lat: 47.575,
+              lng: 7.61,
+              focus: false,
+              message: '<b>Hey</b>, ich bin Bar1',
+              draggable: false
+          },
+          bar2: {
+              lat: 47.575,
+              lng: 7.62,
+              focus: false,
+              message: '<b>Hey</b>, ich bin Bar2',
+              draggable: false
+          }
       },
       position: {
         lat: 47.575,
@@ -37,4 +43,37 @@ angular.module('projekteApp')
       $scope.position.lng = args.model.lng;
     });
 
-} ]);
+  }])
+
+    .controller('dataFeed', ['$scope', '$http', function($scope,$http) {
+
+    var spreadsheet = 'od6';
+    var spreadsheetID = '1ymI_YXjTl1qkOzCTHWTMlwI6T3kc4ix3hhdq9Cfw_Yo';
+    var url = 'http://spreadsheets.google.com/feeds/list/' + spreadsheetID + '/' + spreadsheet + '/public/values?alt=json';
+
+    var parse = function(entry) {
+        console.log(entry);
+        var titel = entry['gsx$titel']['$t'];
+        var inhalt = entry['gsx$inhalt']['$t'];
+        var images = entry['gsx$images']['$t'];
+        return {
+            titel: titel,
+            inhalt: inhalt,
+            images: images
+        };
+    };
+    $http.get(url)
+        .success(function(response) {
+            var key;
+            var entries = response['feed']['entry'];
+            $scope.parsedEntries = [];
+            for (key in entries) {
+                var content = entries[key];
+                $scope.parsedEntries.push(parse(content));
+                console.log(content);
+            }
+        }).error(function (data, status, headers, config) {
+        alert("error");
+        return status;
+    });
+}]);
